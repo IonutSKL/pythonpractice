9861cfbeafb0191241d00ad8d19787f36be263ee
IonutSKL <scarlatalinn@gmail.com>
2019-11-14 23:17:38 +0100

    Update primes.py

--- Exercise Files/Chap02/primes.py
+++ Exercise Files/Chap02/primes.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-def isprime(n):
+
+def is_prime(n):
     if n <= 1:
         return False
     for x in range(2, n):
@@ -10,9 +11,17 @@ def isprime(n):
     else:
         return True
 
+
+def list_primes():
+    for n in range(100):
+        if is_prime(n):
+            print(n, end=" ")
+    print()
+
+list_primes()
+
 n = 5
-if isprime(n):
+if is_prime(n):
     print(f'{n} is prime')
 else:
     print(f'{n} not prime')
-
