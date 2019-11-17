e3b5e23862e0e4d77acb7760485437613bb4d666
IonutSKL <scarlatalinn@gmail.com>
2019-11-17 22:23:08 +0100

    Update types.py
    update types

--- Exercise Files/Chap03/types.py
+++ Exercise Files/Chap03/types.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 7
+x = [1, 2, 3, 4]
 print('x is {}'.format(x))
 print(type(x))
+
+if isinstance(x, tuple):
+    print('is a tuple')
+elif isinstance(x, list):
+    print('is a list')
